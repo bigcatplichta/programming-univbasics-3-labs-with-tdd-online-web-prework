bb662a0cffd1d12ef525aee29c7eb57bfb8390c8
Matt Plichta <bigcatplichta@gmail.com>
2019-08-09 20:44:35 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
-first_number = (integer) or (float)
-second_number = (integer) or (float)
+first_number = 10 
+second_number = 5
 sum = (first_number+second_number)
 difference = (first_number-second_number)
 product = (first_number*second_number)
